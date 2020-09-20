4269e462dce47f9d77a110ba1b8a0cde2c269d22
KwanghyunOn <kwanghyun.on@gmail.com>
2020-09-20 06:24:32 +0000

    hw3

--- hw3/procReport.c
+++ hw3/procReport.c
@@ -19,28 +19,28 @@ const unsigned long addr[6] = {0x5631c25f8918, 0x5631c27f9010, 0x5631c27f902c, 0
 const char *str[6] = {"read-only", "data", "bss", "heap", "stack", "shared library"};
 
 int proc_init (void) {
-  printk(KERN_INFO "proc_report: kernel module initialized\n");
-  printk(KERN_INFO "proc_id,proc_name,contig_pages,noncontig_pages,total_pages\n");
-  print_phys_addr();
-  return 0;
+	printk(KERN_INFO "proc_report: kernel module initialized\n");
+	printk(KERN_INFO "proc_id,proc_name,contig_pages,noncontig_pages,total_pages\n");
+	print_phys_addr();
+	return 0;
 }
 
 void proc_cleanup(void) {
-  printk(KERN_INFO "proc_report: performing cleanup of module\n");
+	printk(KERN_INFO "proc_report: performing cleanup of module\n");
 }
 
 static void print_phys_addr() {
-    struct task_struct *task;
+		struct task_struct *task;
 	for_each_process(task)
 		if(task->pid == PID) break;
 
-    struct vm_area_struct *vma = 0;
-    unsigned long vpage, p_addr;
+		struct vm_area_struct *vma = 0;
+		unsigned long vpage, p_addr;
 	int i;
 
-    if(task->mm && task->mm->mmap)
-        for(vma = task->mm->mmap; vma; vma = vma->vm_next)
-            for(vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE) 
+		if(task->mm && task->mm->mmap)
+				for(vma = task->mm->mmap; vma; vma = vma->vm_next)
+						for(vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE) 
 				for(i = 0; i < 6; i++) 
 					if(vpage <= addr[i] && addr[i] < vpage + PAGE_SIZE) {
 						p_addr = virt2phys(task->mm, vpage);
@@ -49,32 +49,32 @@ static void print_phys_addr() {
 }
 
 static unsigned long virt2phys(struct mm_struct *mm, unsigned long vpage) {
-    pgd_t *pgd;
-    p4d_t *p4d;
-    pud_t *pud;
-    pmd_t *pmd;
-    pte_t *pte;
-    unsigned long physical_page_addr;
-    struct page *page;
-    pgd = pgd_offset(mm, vpage);
-    if (pgd_none(*pgd) || pgd_bad(*pgd))
-        return 0;
-    p4d = p4d_offset(pgd, vpage);
-    if (p4d_none(*p4d) || p4d_bad(*p4d))
-        return 0;
-    pud = pud_offset(p4d, vpage);
-    if (pud_none(*pud) || pud_bad(*pud))
-        return 0;
-    pmd = pmd_offset(pud, vpage);
-    if (pmd_none(*pmd) || pmd_bad(*pmd))
-        return 0;
-    if (!(pte = pte_offset_map(pmd, vpage)))
-        return 0;
-    if (!(page = pte_page(*pte)))
-        return 0;
-    physical_page_addr = page_to_phys(page);
-    pte_unmap(pte);
-    return physical_page_addr;
+		pgd_t *pgd;
+		p4d_t *p4d;
+		pud_t *pud;
+		pmd_t *pmd;
+		pte_t *pte;
+		unsigned long physical_page_addr;
+		struct page *page;
+		pgd = pgd_offset(mm, vpage);
+		if (pgd_none(*pgd) || pgd_bad(*pgd))
+				return 0;
+		p4d = p4d_offset(pgd, vpage);
+		if (p4d_none(*p4d) || p4d_bad(*p4d))
+				return 0;
+		pud = pud_offset(p4d, vpage);
+		if (pud_none(*pud) || pud_bad(*pud))
+				return 0;
+		pmd = pmd_offset(pud, vpage);
+		if (pmd_none(*pmd) || pmd_bad(*pmd))
+				return 0;
+		if (!(pte = pte_offset_map(pmd, vpage)))
+				return 0;
+		if (!(page = pte_page(*pte)))
+				return 0;
+		physical_page_addr = page_to_phys(page);
+		pte_unmap(pte);
+		return physical_page_addr;
 }
 
 MODULE_LICENSE("GPL");
